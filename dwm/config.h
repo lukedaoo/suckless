cb352a5b4d90107dd29748f9f32641a6a9037941
LukeD <locdaohuuit@outlook.com>
2022-09-25 19:41:10 -0700

    default browser dwm

--- dwm/config.h
+++ dwm/config.h
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const char browser[] = "Microsoft-edge";
+static const char browser[] = "Google-chrome"; /* "Microsoft-edge", "Firefox" */
 
 static const Rule rules[] = {
 	/* xprop(1):
