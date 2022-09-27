7b9f9d1af6bec7865b4a9b18b885f3be571fe690
LukeD <locdaohuuit@outlook.com>
2022-09-27 00:08:18 -0700

    [dwm] tag for ferdium

--- dwm/config.h
+++ dwm/config.h
@@ -34,6 +34,7 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const char browser[] = "Google-chrome"; /* "Microsoft-edge", "Firefox" */
+static const char ferdium[] = "Ferdium";
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -43,6 +44,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ browser,  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ ferdium,  NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
