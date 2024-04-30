25a749da7fc8af553cb313d2341d089ddffb49ce
LD-arch-t470p <locdaohuuit@outlook.com>
2024-04-30 13:05:36 -0700

    stop being stupid

--- dwm/config.h
+++ dwm/config.h
@@ -165,7 +165,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_minus,      quit,           {0} },
 	{ MODKEY,                       XK_n,      spawn,           {.v = take_note} },
 	{ MODKEY,                       XK_F4,      spawn,           {.v = screenshot} },
 	{ MODKEY,                       XK_Print,      spawn,           {.v = screenshot} },
