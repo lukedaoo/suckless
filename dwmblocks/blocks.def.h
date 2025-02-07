c1bee07cdae8c5ce30aa909ac64cec74c3e5cdf6
luked <locdaohuuit@outlook.com>
2025-02-06 23:42:54 -0800

    remove sb-music

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,18 +1,20 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "sb-music", 0, 16},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    // {"", "sb-music", 1, 16},
     {"", "sb-netspeed", 5, 17},
-	{"", "sb-cpu", 5, 18},
-	{"", "sb-memory", 5, 19}, 
+    {"", "sb-cpu", 5, 18},
+    {"", "sb-memory", 5, 19},
     {"", "sb-volume", 3, 20},
-    {"", "sb-battery", 3, 21},
+    // {"", "sb-battery", 3, 21},
     {"", "sb-bluetooth", 5, 23},
     {"", "sb-internet", 5, 24},
-	{"", "sb-date", 1, 25},
-	{"", "sb-user", 0, 26},
+    {"", "sb-date", 1, 25},
+    {"", "sb-user", 0, 26},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
